b84003dbfbb2381300f98256eb8a2949900e63d7
Hrushikesh1430 <hdtawde7@gmail.com>
2020-09-24 18:11:14 +0530

    first commit

---
+++ javascript/portfolio.js
@@ -0,0 +1,15 @@
+AOS.init();{
+    
+}
+
+var typed = new Typed(".type", {
+    strings:[
+    "Coder",
+    "Musician",
+    "Writer"
+  ],
+  smartbackspace:true,
+  typeSpeed:50,
+  backSpeed:50,
+  loop:true
+});
