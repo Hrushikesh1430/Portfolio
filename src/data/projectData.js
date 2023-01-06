39afc0c6e35faf0f50a1a0306d3202356c6b3fbb
Hrushikesh Tawde <hdtawde7@gmail.com>
2023-01-06 18:59:46 +0530

    Project update

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,10 +1,10 @@
 export const projects = [
   {
     id: 1,
-    img: "expense-tracker.png",
-    link: "https://my-expense-track.netlify.app/",
-    title: "Expense Tracker with React JS",
-    source: "https://github.com/Hrushikesh1430/ExpenseTracker",
+    img: "nayaki.png",
+    link: "https://nayaki.netlify.app/",
+    title: "Nayaki",
+    source: "https://github.com/Hrushikesh1430/FoodApp",
   },
   {
     id: 2,
@@ -22,59 +22,53 @@ export const projects = [
   },
   {
     id: 4,
+    img: "expense-tracker.png",
+    link: "https://my-expense-track.netlify.app/",
+    title: "Expense Tracker with React JS",
+    source: "https://github.com/Hrushikesh1430/ExpenseTracker",
+  },
+  {
+    id: 5,
+    img: "cash-register.png",
+    link: "https://cash-register-app-04.netlify.app/",
+    title: "Cash Register App",
+    source: "https://github.com/Hrushikesh1430/Cash-Register",
+  },
+
+  {
+    id: 6,
     img: "minion.png",
     link: "https://minion-translator-82.netlify.app/",
     title: "Minion Translator App",
     source: "https://github.com/Hrushikesh1430/Minion-Translator-App",
   },
   {
-    id: 5,
+    id: 7,
     img: "pirate.png",
     link: "https://pirate-translator-app-82.netlify.app/",
     title: "Pirate Translator App",
     source: "https://github.com/Hrushikesh1430/Pirate-Translator-App",
   },
+
   {
-    id: 6,
-    img: "nayaki.png",
-    link: "https://nayaki.netlify.app/",
-    title: "Nayaki",
-    source: "https://github.com/Hrushikesh1430/FoodApp",
-  },
-  {
-    id: 7,
+    id: 8,
     img: "emoji.png",
     link: "https://eu208y.csb.app/",
     title: "Emoji to Text Converter App",
     source: "https://codesandbox.io/s/eu208y",
   },
   {
-    id: 8,
+    id: 9,
     img: "bollywood.png",
     link: "https://b7mifk.csb.app/",
     title: "Bollywood Movie Recommender App",
     source: "https://codesandbox.io/s/b7mifk",
   },
   {
-    id: 9,
+    id: 10,
     img: "friends.png",
     link: "https://replit.com/@HrushikeshTawde/MarkOneProjectFriends?v=1",
     title: "Friends Quiz App",
     source: "https://replit.com/@HrushikeshTawde/MarkOneProjectFriends?v=1",
   },
-  //   {
-  //     id: 4,
-  //     img: "",
-  //     link: "https://github.com/Hrushikesh1430",
-  //   },
-  //   {
-  //     id: 5,
-  //     img: "",
-  //     link: "https://github.com/Hrushikesh1430",
-  //   },
-  //   {
-  //     id: 6,
-  //     img: "",
-  //     link: "https://github.com/Hrushikesh1430",
-  //   },
 ];
