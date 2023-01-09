18e0e58d432424f3df0a6c3146e3b72d5b75e0ac
Hrushikesh Tawde <hdtawde7@gmail.com>
2023-01-09 13:07:19 +0530

    Removed warnings

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,6 +1,5 @@
 import Intro from "../Intro/Intro";
 import About from "../About/About";
-import ProjectList from "../ProjectList/ProjectList";
 import Contact from "../Contact/Contact";
 import React from "react";
 
